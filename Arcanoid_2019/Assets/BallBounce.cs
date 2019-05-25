d48f9dc8207814aec4242fc5139ab96a867184f2
Kowallukasz01 <Kowallukasz01@gmail.com>
2019-05-25 19:02:55 +0200

    Creating project

---
+++ Arcanoid_2019/Assets/BallBounce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(Vector2.up * power);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Arcanoid_2019/Assets/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float Speed;
+    public float RightScreenEdge;
+    public float LeftScreenEdge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * Speed);
+        if (transform.position.x < LeftScreenEdge)
+        {
+            transform.position = new Vector2(LeftScreenEdge, transform.position.y);
+        }
+
+        if (transform.position.x > RightScreenEdge)
+        {
+            transform.position = new Vector2(RightScreenEdge, transform.position.y);
+        }
+    }
+}
