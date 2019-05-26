6dfa0454ecb8f299bf3a4b919dbc829336748366
Kowallukasz01 <Kowallukasz01@gmail.com>
2019-05-26 15:15:20 +0200

    Added ball bouncing; left, right and top triggers; set starting possition

--- Arcanoid_2019/Assets/BallBounce.cs
+++ Arcanoid_2019/Assets/BallBounce.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BallBounce : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float power;
+    public bool isInPlay;
+    public Transform paddle;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        rb.AddForce(Vector2.up * power);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isInPlay)
+        {
+            transform.position = paddle.position;
+        }
+
+        if (Input.GetButtonDown("Jump") && !isInPlay)
+        {
+            isInPlay = true;
+            rb.AddForce(Vector2.up * power);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bottom"))
+        {
+            Debug.Log("ball has hitted a bottom");
+
+            rb.velocity = Vector2.zero;
+            isInPlay = false;
+        }
     }
 }
